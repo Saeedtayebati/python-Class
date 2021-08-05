225769ccb583316e6c2e8b35a468dbdec7152dd7
Saeedtayebati <87672447+Saeedtayebati@users.noreply.github.com>
2021-08-05 11:15:07 +0430

    Create S2_0_Calculate.py

---
+++ S2_0_Calculate.py
@@ -0,0 +1,11 @@
+a=int(input('Addad 1'))
+b=int(input('Addad 2'))
+c=input()
+if  c == "+":
+   print(a+b)
+elif c == "*" :
+    print (a*b)
+elif c=="-" :
+    print(a-b)
+else :
+    print("balda No")
