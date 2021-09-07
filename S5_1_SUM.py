1ee6e23e222e2c63905fe33e0376f046db075af6
Saeedtayebati <87672447+Saeedtayebati@users.noreply.github.com>
2021-09-07 14:49:10 +0430

    Create S5_1_SUM.py

---
+++ S5_1_SUM.py
@@ -0,0 +1,14 @@
+numbers=list()
+
+while 1==1:
+    adad=int(input("vared kon?"))
+    if adad<0:
+        break
+
+    numbers.append(adad)
+print(numbers)
+a=len(numbers)
+b=sum(numbers)
+print(b)
+c=b/a
+print(c)
