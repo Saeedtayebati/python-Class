cfcb1c7aa8148bd53df4070a73664de452ade86f
Saeedtayebati <87672447+Saeedtayebati@users.noreply.github.com>
2021-09-27 12:18:33 +0330

    Create S8_3_Class.py

---
+++ S8_3_Class.py
@@ -0,0 +1,13 @@
+class Animal():
+    def __init__(self,user_name,user_nejad,user_natinal):
+
+
+       self.name=user_name
+       self.nejad=user_nejad
+       self.natinal=user_natinal
+
+
+palang=Animal('Palang','Goski','America')
+Ahoo=Animal('Ahoo','jojo','Africa')
+
+print(palang.natinal)
