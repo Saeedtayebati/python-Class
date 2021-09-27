7ddae58235dfa0c1b9a357b13ebd264578e39bdc
Saeedtayebati <87672447+Saeedtayebati@users.noreply.github.com>
2021-09-27 12:16:05 +0330

    Create S8_0_Class.py

---
+++ S8_0_Class.py
@@ -0,0 +1,13 @@
+class Person():
+    def __init__(self,user_name,user_lastname,user_age):
+
+
+       self.name=user_name
+       self.lastname=user_lastname
+       self.age=user_age
+
+
+ali=person('Ali','Gholi',23)
+saeed=person('Saeed','Tayebati',29)
+
+print(saeed.lastname)
