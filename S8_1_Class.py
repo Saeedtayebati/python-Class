90cc795d0fd1d9f0026f73500eb60c988ccd233e
Saeedtayebati <87672447+Saeedtayebati@users.noreply.github.com>
2021-09-27 12:16:44 +0330

    Create S8_1_Class.py

---
+++ S8_1_Class.py
@@ -0,0 +1,13 @@
+class Car():
+    def __init__(self,user_name,user_power,user_Madin):
+
+
+       self.name=user_name
+       self.power=user_power
+       self.Madin=user_Madin
+
+
+bmw=Car('BMW',2300,'German')
+benz=Car('Benz',4500,'German')
+
+print(bmw.power)
