959ac1749bc8fe33ddd58f506f70ef41ab3e9c7f
Saeedtayebati <87672447+Saeedtayebati@users.noreply.github.com>
2021-09-27 12:17:08 +0330

    Create S8_2_Class.py

---
+++ S8_2_Class.py
@@ -0,0 +1,13 @@
+class Computer():
+    def __init__(self,user_name,user_power,user_cpu):
+
+
+       self.name=user_name
+       self.power=user_power
+       self.cpu=user_cpu
+
+
+pc=Computer('PC',46,'intel')
+saeed=person('Laptop',29,'Asus')
+
+print(pc.cpu)
