0d5383555108ba8ccaaa99b231ca86819800dfcc
Saeedtayebati <87672447+Saeedtayebati@users.noreply.github.com>
2021-10-09 11:05:36 +0330

    Create S9_0_Class.py

---
+++ S9_0_Class.py
@@ -0,0 +1,10 @@
+Class bird():
+    def__init__(self,Nejad,model,color):
+        self.Nejad=Nejad
+        self.model=model
+        self.color=color
+    def Avaz(self):
+        print('baghbagh')
+my_new_bird=bird('Pestandar','Toti','Blue')
+print(my_new_bird.model,color)
+my_new_bird.Avaz()
